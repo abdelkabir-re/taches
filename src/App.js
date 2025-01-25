c64b2e05fee90128191d65093906d1640962341a
abdelkabir-re <abdelkabirrabya@gmail.com>
2025-01-25 22:59:45 +0100

    start my project

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState,useEffect } from 'react';
+import { Route,Routes,Link } from 'react-router-dom';
+import Works from './components/Works';
+function App() {
+  const [taches,setTaches]=useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users/1/todos')
+    .then((response)=>{
+      return response.json()
+    }).then((result)=>{
+      setTimeout(()=>{
+        setTaches(result)
+       },2000)
+    }).catch((e)=>{
+      console.log(e)
+    })
+  },[])
+  return (
+    <div className="App">
+        <h1>Gestion de Projet</h1>
+        <Routes>
+          <Route path='/' element={<Works/>}/>
+        </Routes>
+        
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Works.js
@@ -0,0 +1,5 @@
+export default function Works(){
+    return (
+        <h1>hello</h1>
+    )
+}
